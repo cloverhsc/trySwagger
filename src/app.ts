26a495c67d983f0446e0e58581157cf91936a377
Clover Hsu <cloverh@supermicro.com>
2024-05-07 11:18:26 +0800

    add required libraries and setup the first swagger ui express

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,30 @@
-console.log('hello world');
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
+import express from 'express';
+import cors from 'cors';
 
-console.log('Hi Clover');
+const ports = process.env.Port || 3000;
+const router = express.Router();
+
+// cors settings
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+};
+
+// create express app
+const app = express();
+
+// set cors
+app.use(cors(corsOptions));
+
+// set router
+app.use('/', router);
+
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+// assign port
+app.listen(ports, () => {
+  console.log(`Server is listening on port ${ports}`);
+});
