1e0bdde410f5a9c3e74d1c7e217d2ae77b0adfe1
Clover Hsu <cloverh@supermicro.com>
2024-05-13 19:45:10 +0800

    Import YAML configuration file

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from './swagger.json';
+// import swaggerDocument from './swagger.json';
+import YAML from 'yamljs';
 import express from 'express';
 import cors from 'cors';
 
+
 const ports = process.env.Port || 3000;
 const router = express.Router();
+
+
+// load swagger.yaml
+
+const swaggerDocument = YAML.load(__dirname + '/swagger.yml');
+console.log(swaggerDocument)
+
 
 // cors settings
 const corsOptions = {
@@ -20,7 +29,7 @@ const app = express();
 app.use(cors(corsOptions));
 
 // set router
-app.use('/', router);
+// app.use('/', router);
 
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
